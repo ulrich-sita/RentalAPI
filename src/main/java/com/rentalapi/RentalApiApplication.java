fc0fb9e98763e5e09fb1774438d252c5f2b979a8
ulrich-sita <ulrich.sita12@gmail.com>
2024-07-25 20:03:21 +0200

    Initial commit with project setup files

---
+++ src/main/java/com/rentalapi/RentalApiApplication.java
@@ -0,0 +1,13 @@
+package com.rentalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalApiApplication.class, args);
+	}
+
+}
